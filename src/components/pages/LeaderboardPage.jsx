ba773d8b469ed4b7dfcf482aa6bada8f747f8a7c
Omar yassen <88896705+Omaryassenn@users.noreply.github.com>
2023-08-03 14:46:49 +0300

    remove some redundancy

--- src/components/pages/LeaderboardPage.jsx
+++ src/components/pages/LeaderboardPage.jsx
@@ -3,7 +3,7 @@ import Rating from "@components/Rating";
 // import humanFormat from "human-format";
 import Podium from "@components/Podium";
 import PropTypes from "prop-types";
-
+import './Leaderboard.css'
 const LeaderboardPage = ({ leaderboardDetails }) => {
   leaderboardDetails = leaderboardDetails.sort((a, b) => b.points - a.points);
   const topThree = leaderboardDetails.slice(0, 3).map((user) => ({
@@ -12,17 +12,17 @@ const LeaderboardPage = ({ leaderboardDetails }) => {
 
   return (
     <div className="relative container h-full py-8 px-6">
-      <span className="fixed inline-block w-1/3 h-full -translate-x-1/2 left-1/2 rounded-full bg-[#0109D3] opacity-[0.6] blur-[500px]"></span>
+      <span className="fixed inline-block w-1/3 h-full -translate-x-1/2 left-1/2 rounded-full bg-[#0109D3] opacity-[0.7] blur-[500px]"></span>
       <Podium
-        className="w-1/2 mx-auto"
+        className="w-1/2 mx-auto "
         winners={topThree}
         colors={["#FEDE00", "#009BD6", "#00D95F"]}
       />
 
-      <div className="w-full h-full flex flex-col items-start gap-2 py-8">
+      <div className="w-full h-full flex flex-col items-start  " id="x">
         {leaderboardDetails.slice(3).map((user, index) => (
           <div
-            className="flex gap-3 items-center w-[80%] mx-auto pb-2"
+            className="flex gap-4 items-center w-[90%] mx-auto p-3"
             key={index}
             style={{
               borderBottom: `2px solid rgba(95, 89, 89, 0.54)`,
