2f82885debac0f9d59ec18c543474b8a2d9eab83
adhamali450 <adhamali_4500@outlook.com>
2023-08-15 19:39:07 +0200

    :fire: Added TechnicalIssuePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Fragment } from "react";
 import { Route, Routes } from "react-router";
 import HomePage from "@pages/HomePage";
 import MembersPage from "@pages/MembersPage";
 import GalleryPage from "@pages/GalleryPage";
 import LeaderboardPage from "@pages/LeaderboardPage";
+import TechnicalIssuePage from "@pages/TechnicalIssuePage";
 import SideDrawer from "@components/SideDrawer";
 
 import "./App.sass";
@@ -2805,23 +2807,34 @@ function App() {
     };
   });
 
+  const technicalIssue = true;
   return (
     <div className="relative App flex flex-col md:flex-row">
-      <SideDrawer className="fixed z-50 md:py-8 bg-[#333333] md:bg-[#3333338a] w-[100vw] h-auto md:w-auto md:h-[100vh]" />
+      {(technicalIssue && <TechnicalIssuePage />) || (
+        <Fragment>
+          <SideDrawer className="fixed z-50 md:py-8 bg-[#333333] md:bg-[#3333338a] w-[100vw] h-auto md:w-auto md:h-[100vh]" />
 
-      <div className="relative wrapper w-full  h-[calc(100vh-64px)] md:h-[100vh] mt-[64px] md:mt-0 md:mr-[95px] xmd:mr-[140px]">
-        <Routes>
-          <Route path="/" element={<HomePage details={details} />} />
-          <Route path="/members" element={<MembersPage members={members} />} />
-          <Route path="/gallery" element={<GalleryPage images={images} />} />
-          <Route
-            path="/leaderboard"
-            element={
-              <LeaderboardPage leaderboardDetails={leaderboardDetails} />
-            }
-          />
-        </Routes>
-      </div>
+          <div className="relative wrapper w-full  h-[calc(100vh-64px)] md:h-[100vh] mt-[64px] md:mt-0 md:mr-[95px] xmd:mr-[140px]">
+            <Routes>
+              <Route path="/" element={<HomePage details={details} />} />
+              <Route
+                path="/members"
+                element={<MembersPage members={members} />}
+              />
+              <Route
+                path="/gallery"
+                element={<GalleryPage images={images} />}
+              />
+              <Route
+                path="/leaderboard"
+                element={
+                  <LeaderboardPage leaderboardDetails={leaderboardDetails} />
+                }
+              />
+            </Routes>
+          </div>
+        </Fragment>
+      )}
     </div>
   );
 }
---
+++ src/components/pages/TechnicalIssuePage.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import logo from "@assets/logo.svg";
+import iconInstagram from "@icons/instagram.svg";
+import grimReaper from "@icons/grim-reaper.svg";
+
+const TechnicalIssuePage = () => {
+  return (
+    <div className="relative w-full h-[100vh] flex items-center justify-center flex-col gap-4">
+      <img className="w-[250px]" src={logo} alt="حمامة شيراتون" />
+      <h1
+        className="text-2xl"
+        style={{
+          lineHeight: "2.3rem",
+        }}
+      >
+        باسل بوظ الموقع غصب عنه 😞
+        <br />
+        بنحاول نحل الموضوع دلوقتي
+      </h1>
+      <a
+        className="flex items-center mt-5 gap-2 px-5 py-2 rounded-md"
+        href="https://www.instagram.com/fcisbroskies/"
+        target="_blank"
+        rel="noreferrer"
+        style={{
+          background:
+            "linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d)",
+        }}
+      >
+        <img className="w-8 h-8" src={iconInstagram} />
+        fcisbroskies
+      </a>
+
+      <img
+        className="absolute bottom-[-5%] right-0 min-w-[350px] w-[40vw] -z-10 opacity-30"
+        src={grimReaper}
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default TechnicalIssuePage;
